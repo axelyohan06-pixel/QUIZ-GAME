ce767c837bc6e4ede68ee5b7dd5c9b38da171777
Achie Yohan Joseph Axel <yohanaxel@192.168.1.21>
2026-03-15 22:12:07 +0000

    Feat: Add cache busting to force updates on client browsers

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // Utilise des chemins relatifs pour fonctionner sur GitHub Pages
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-[hash]-${Date.now()}.[ext]`
+      }
+    }
+  }
 })
